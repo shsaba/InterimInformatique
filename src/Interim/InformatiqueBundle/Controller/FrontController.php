e8a5cb55dc818b18f8c8cf2261502f74df5557f7
ShySaba <shyam.sabapathi@gmail.com>
2014-03-23 20:58:09 +0100

    Modification de l'héritage des views.

--- src/Interim/InformatiqueBundle/Controller/FrontController.php
+++ src/Interim/InformatiqueBundle/Controller/FrontController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FrontController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('InterimInformatiqueBundle:Front:index.html.twig', array('name' => $name));
+        return $this->render('InterimInformatiqueBundle:Front:index.html.twig', array());
     }
 }
