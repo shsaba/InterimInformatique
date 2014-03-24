9d7635d66a35161f8612491c558d5f9fde017c84
shysaba <shysaba@gmail.com>
2014-03-24 16:24:28 +0100

    Création de forms tests

--- src/Interim/InformatiqueBundle/Controller/FrontController.php
+++ src/Interim/InformatiqueBundle/Controller/FrontController.php
@@ -3,11 +3,33 @@
 namespace Interim\InformatiqueBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Interim\InformatiqueBundle\Entity\Diploma;
+use Interim\InformatiqueBundle\Form\DiplomaType;
 
 class FrontController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('InterimInformatiqueBundle:Front:index.html.twig', array());
+
+    public function indexAction() {
+
+        $diplomaLevel = new Diploma();
+
+        $form = $this->createForm(new DiplomaType, $diplomaLevel);
+
+        $request = $this->getRequest();
+
+        if ($request->getMethod() == "POST") {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($diplomaLevel);
+                $em->flush();
+            }
+        }
+        return $this->render('InterimInformatiqueBundle:Front:index.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
+
 }
