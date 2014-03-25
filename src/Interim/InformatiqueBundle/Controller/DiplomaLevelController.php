0b62f5cf959d5d13e32ca221a1c70684775fd697
ShySaba <shyam.sabapathi@gmail.com>
2014-03-25 22:24:50 +0100

    Ajout des fonctionnalités pour les niveaux de diplomes

---
+++ src/Interim/InformatiqueBundle/Controller/DiplomaLevelController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Interim\InformatiqueBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Interim\InformatiqueBundle\Entity\DiplomaLevel;
+use Interim\InformatiqueBundle\Form\DiplomaLevelType;
+
+
+class DiplomaLevelController extends Controller
+{
+
+    public function indexAction() {
+
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('InterimInformatiqueBundle:DiplomaLevel');
+
+        $diplomasLevels = $repo->findAll();
+
+        return $this->render('InterimInformatiqueBundle:DiplomaLevel:index.html.twig', array(
+                    'diplomasLevels' => $diplomasLevels
+        ));
+    }
+
+    public function addAction() {
+
+        $diplomaLevel = new DiplomaLevel();
+
+        $form = $this->createForm(new DiplomaLevelType, $diplomaLevel);
+        $request = $this->getRequest();
+
+        if ($request->getMethod() == "POST") {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($diplomaLevel);
+                $em->flush();
+                $this->get('session')->getFlashBag()->add('info', 'Le niveau de diplômes a bien été enregistré');
+                return $this->redirect($this->generateUrl('interim_informatique_configuration_diplomas_levels'));
+            }
+        }
+        return $this->render('InterimInformatiqueBundle:DiplomaLevel:add.html.twig', array(
+                    'form' => $form->createView()
+                        )
+        );
+    }
+
+}
